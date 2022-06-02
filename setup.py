762ecd369f64efcddf3700c806e0f34adcca339e
Paul LaMendola <paulguy119@gmail.com>
2022-06-02 01:07:55 -0400

    Mac OS support.

--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
+import os
 from setuptools import setup, Extension
 
 crustygame = Extension('crustygame',
                        include_dirs = ['/usr/include/SDL2'],
-                       libraries = ['SDL2'],
                        sources = ['log_cb_helper.c', 'tilemap.c', 'synth.c', 'crustygamemodule.c'],
                        extra_compile_args = ['-W', '-Wall', '-Wextra', '-Wno-unused-parameter', '-DCOPY_FIX'])
+
+if os.uname().sysname == 'Darwin':
+    crustygame.include_dirs = ["/Library/Frameworks/SDL2.framework/Headers"]
+    crustygame.extra_link_args = ["-F", "/Library/Frameworks", "-framework", "SDL2"]
+else:
+    crustygame.libraries = ['SDL2']
 
 setup (name = 'crustygame',
        version = '1.0',
